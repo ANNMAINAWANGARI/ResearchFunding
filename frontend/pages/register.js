aa6a488d5c58d67c07f7e887d574b70391010081
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-02-01 14:21:25 +0300

    register page frontend

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,17 +1,24 @@
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Container,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { useRouter } from "next/router";
 
 const register = () => {
-    const router = useRouter();
+  const router = useRouter();
   return (
     <div>
       <AppBar
         position="sticky"
         elevation={0}
         color="transparent"
-        sx={{ height: "10vh" }}
+        sx={{ height: "10vh", marginBottom: 6 }}
       >
         <Container maxWidth="lg" sx={{ paddingLeft: 0, paddingRight: 0 }}>
           <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
@@ -48,6 +55,31 @@ const register = () => {
           </Toolbar>
         </Container>
       </AppBar>
+      <Typography variant="h4" align="center" gutterBottom>
+        Register your research Organization
+      </Typography>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "650px",
+          alignItems: "center",
+          justifyContent: "center",
+          marginLeft:'auto',
+          marginRight:'auto',
+          //border:'1px solid #00A86B'
+        }}
+      >
+        <TextField id="outlined-address" label="Address" fullWidth sx={{margin:1}}/>
+        <TextField id="outlined-organization" label="Organization" fullWidth sx={{margin:2}}/>
+        <TextField id="outlined-country" label="Country" fullWidth sx={{margin:2}}/>
+        <TextField id="outlined-fund" label="FundingPurpose" fullWidth sx={{margin:2}}/>
+        <TextField id="outlined-amount" label="AmountRequested" fullWidth sx={{margin:2}}/>
+        <TextField id="outlined-amtRaised" label="AmountRaised" fullWidth sx={{margin:2}}/>
+      </form>
+      <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <Button variant='contained'sx={{backgroundColor:'#00A86B'}}>Submit</Button>
+      </Box>
     </div>
   );
 };
