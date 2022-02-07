710abb8acd62cda68ecf8531651acbfa342d9a59
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-02-07 23:29:45 +0300

    added functionality to the register form

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -7,11 +7,19 @@ import {
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
 import { useRouter } from "next/router";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
 import { RESEARCHFUNDING_CONTRACT_ADDRESS, abi } from "../constants";
 
 const register = () => {
+  const [address,setAddress]=useState('');
+  const [organization,setOrganization]=useState('');
+  const [country,setCountry]=useState('');
+  const [fundingPupose,setFundingPurpose]=useState('');
+  const [amountRequested,setAmountRequested]=useState('');
+  const [amountRaised,setAmountRaised]=useState('');
   useEffect(()=>{
     if (typeof window.ethereum == 'undefined') {
       console.log('MetaMask is not installed!');
@@ -25,13 +33,13 @@ const register = () => {
       }
     }
     
-  })
+  },[])
   const router = useRouter();
   return (
     <div>
       <AppBar
         position="sticky"
-        elevation={1}
+        elevation={3}
         color="transparent"
         sx={{ height: "10vh", marginBottom: 6 }}
       >
@@ -89,35 +97,47 @@ const register = () => {
           id="outlined-address"
           label="Address"
           fullWidth
+          value={address}
+          onChange={(e)=>setAddress(e.target.value)}
           sx={{ margin: 1 }}
         />
         <TextField
           id="outlined-organization"
           label="Organization"
           fullWidth
+          value={organization}
+          onChange={(e)=>setOrganization(e.target.value)}
           sx={{ margin: 2 }}
         />
         <TextField
           id="outlined-country"
           label="Country"
           fullWidth
+          value={country}
+          onChange={(e)=>setCountry(e.target.value)}
           sx={{ margin: 2 }}
         />
         <TextField
           id="outlined-fund"
           label="FundingPurpose"
+          value={fundingPupose}
+          onChange={(e)=>setFundingPurpose(e.target.value)}
           fullWidth
           sx={{ margin: 2 }}
         />
         <TextField
           id="outlined-amount"
           label="AmountRequested"
+          value={amountRequested}
+          onChange={(e)=>setAmountRequested(e.target.value)}
           fullWidth
           sx={{ margin: 2 }}
         />
         <TextField
           id="outlined-amtRaised"
           label="AmountRaised"
+          value={amountRaised}
+          onChange={(e)=>setAmountRaised(e.target.value)}
           fullWidth
           sx={{ margin: 2 }}
         />
