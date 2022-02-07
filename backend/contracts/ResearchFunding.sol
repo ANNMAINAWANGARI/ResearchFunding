d583f66ef62977b4fb3062d10762d3883de310cf
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-02-07 21:40:32 +0300

    deployed smart contract

--- backend/contracts/ResearchFunding.sol
+++ backend/contracts/ResearchFunding.sol
@@ -1,68 +1,55 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "../node_modules/hardhat/console.sol";
-import "@openzeppelin/contracts/access/Roles.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract ResearchFunding {
-     using Roles for Roles.Role;
-    /** @dev GRO:Global Research Organization**/
-     Roles.Role private _GRO;
-     Roles.Role private _ResearchOrg;
-
-/**  @dev think of it like an object that stores each organizations metadata**/
-     struct ROrganizations{
-         string orgName;
-         string orgCountry;
-         string fundingPurpose;
-         uint256 amtNeeded;
-         uint256 amtRaised;
+contract ResearchFunding is AccessControl, ReentrancyGuard {
+    bytes32 private constant GRO_ROLE = keccak256("GRO_ROLE");
+    bytes32 private constant RESEARCHORG_ROLE = keccak256("RESEARCHORG_ROLE");
+    constructor(){
+    _setupRole(GRO_ROLE, msg.sender);
 }
-/** @dev mapping that maps each organization address to the struct ROrganizations**/
-mapping(address => ROrganizations) organization;
-
-
-/** @dev address array that stores research organization accounts**/ 
+struct ROrganizations{
+ string orgName;
+ string orgCountry;
+ string fundingPurpose;
+ uint256 amtNeeded;
+ uint256 amtRaised;
+}
+mapping(address => ROrganizations) organisation;
 address[] public orgAccts;
-
-
-/** @dev donate event that will be emmited from the frontend**/
 event donate(address, uint256); 
-/** @dev it is the first line of code that is run when the contract compiles.add() is a built in function from openzepplin/contracts/access/roles */
-constructor() public{
-_GRO.add(msg.sender);
-}    
-function addOrg(address _address, string memory _orgName, string memory _orgCountry, string memory _fundingPurpose, uint256 _amtNeeded){
-    /**
-     * @dev Check if an account has this role.
-     */
-     require(_GRO.has(msg.sender),"!ONLY THE GRO CAN ADD ORGANIZATIONS");
-     ROrganizations storage org = organization[_address];
+    
+function addOrg(address _address, string memory _orgName, string memory _orgCountry, string memory _fundingPurpose, uint256 _amtNeeded) public{
+     require(hasRole(GRO_ROLE, msg.sender),"!ONLY THE GRO CAN ADD ORGANIZATIONS");
+     ROrganizations storage org = organisation[_address];
      org.orgName = _orgName;
      org.orgCountry = _orgCountry;
      org.fundingPurpose = _fundingPurpose;
      org.amtNeeded = _amtNeeded;
      org.amtRaised = 0;
-     _ResearchOrg.add(_address);
+     _setupRole(RESEARCHORG_ROLE, _address);
      orgAccts.push(_address);
 }
 function getOrgs() view public returns(address[] memory){
     return orgAccts;
 }
-function getOrg() view public returns(address, string memory, string memory, string memory, uint256, uint256 ){
+function getOrg( address _address) view public returns(string memory, string memory, string memory, uint256, uint256 ){
 return(organisation[_address].orgName,organisation[_address].orgCountry,organisation[_address].fundingPurpose,organisation[_address].amtNeeded,organisation[_address].amtRaised);
 }
-function donateFunds(address _toOrg, uint256 _amount) external payable{
+function donateFunds(address _toOrg) external payable{
     organisation[_toOrg].amtRaised += msg.value;
     emit donate(_toOrg, msg.value);
 }
 function getBalance() public view returns(uint256){
     return address(this).balance;
 }
-function withdrawFunds(address _toOrg) public{
-    require(_ResearchOrg.has(msg.sender),"!YOU ARE NOT PERMITTED TO WITHDRAW FUNDS, PLEASE USE THE CORRECT ADDRESS");
-    uint256 _amtRaised = organization[_toOrg].amtRaised;
-    _toOrg.call.value(_amtRaised)("");
-    organization[_toOrg].amtRaised = 0;
+function withdrawFunds(address _toOrg) public nonReentrant{
+    require(hasRole(RESEARCHORG_ROLE,msg.sender),"!YOU ARE NOT PERMITTED TO WITHDRAW FUNDS, PLEASE USE THE CORRECT ADDRESS");
+    uint256 _amtRaised = organisation[_toOrg].amtRaised;
+    organisation[_toOrg].amtRaised = 0;
+    _toOrg.call{value:_amtRaised}("");
 }
 }
